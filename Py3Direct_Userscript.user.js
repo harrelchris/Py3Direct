12f27b760277046d468fcb7c56e7c33ea0bb36c7
KimPeek <kimpeek@users.noreply.github.com>
2017-06-01 16:45:28 -0500

    Update Py3Direct_Userscript.user.js

--- Py3Direct_Userscript.user.js
+++ Py3Direct_Userscript.user.js
@@ -1,15 +1,15 @@
 // ==UserScript==
 // @name         Py3Direct
 // @name:en      Py3Direct
-// @namespace    https://greasyfork.org/en/users/88414-chris-harrel
-// @description  Replaces text in the url to automatically redirect from Py 2 docs to Py 3
 // @version      0.3
-// @author       Chris Harrel // harrechris@gmail.com // github.com/harrelchris
+// @description  Replaces text in the url to automatically redirect from Py 2 docs to Py 3
+// @namespace    https://greasyfork.org/en/scripts/26235-py3direct
+// @author       https://github.com/kimpeek
 // @include      *docs.python.org/*
 // ==/UserScript==
 
 if (/2\.\d?/.test(window.location.href)){
-    window.location = window.location.href.replace(/2\.\d?/, '3.5');
+    window.location = window.location.href.replace(/2\.\d?/, '3');
 } else if (window.location.href.includes('docs.python.org/2/')){
     window.location = window.location.href.replace('docs.python.org/2/', 'docs.python.org/3/');
 }
