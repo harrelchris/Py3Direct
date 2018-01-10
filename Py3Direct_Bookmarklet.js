bdf809e55c956f8800bd3e59a53391da1370af28
KimPeek <kimpeek@users.noreply.github.com>
2018-01-09 21:51:07 -0600

    Update Py3Direct_Bookmarklet.js

--- Py3Direct_Bookmarklet.js
+++ Py3Direct_Bookmarklet.js
@@ -1,7 +1,7 @@
 javascript:( 
     function main(){ 
         if ((/2\.\d?/.test(window.location.href)) && (window.location.href.includes('docs.python.org'))){
-            window.location = window.location.href.replace(/2\.\d?/, '3.5');
+            window.location = window.location.href.replace(/2\.\d?/, '3');
         } else if (window.location.href.includes('docs.python.org/2/')){
             window.location = window.location.href.replace('docs.python.org/2/', 'docs.python.org/3/');
         } else { 
